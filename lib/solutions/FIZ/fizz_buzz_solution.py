39fae77b19bdce147c1ea714359077974419b8b6
sbx_user1059 <sbx_user1059@ip-10-128-128-221.eu-west-2.compute.internal>
2018-11-25 13:17:30 +0000

    Sun Nov 25 13:17:30 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,5 +1,6 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
+    isOdd=number%2==0
     dlx1=number>10
     dlx2=True
     ns=str(number)
@@ -10,26 +11,36 @@ def fizz_buzz(number):
             break
     div3=number%3==0
     div5=number%5==0
-    dlx= dlx1 and dlx2
+    dlx3= dlx1 and dlx2
+    dlx= dlx3 and isOdd
     in3=str(3) in str(number)
-    in5=str(5) in str(number)	
+    in5=str(5) in str(number)
     if (div3 or in3) and (div5 or in5):
         if dlx:
+            return "fizz buzz fake deluxe"
+        elif dlx3:
             return "fizz buzz deluxe"
         else:
             return "fizz buzz"
     elif div3 or in3:
         if dlx:
+            return "fizz fake deluxe"
+        elif dlx3:
             return "fizz deluxe"
         else:
             return "fizz"
     elif div5 or in5:
         if dlx:
+            return "buzz fake deluxe"
+        elif dlx3:
             return "buzz deluxe"
         else:
             return "buzz"
     elif dlx:
-        return "deluxe"
+        if isOdd:
+            return "fake deluxe"
+        else:
+            return "deluxe"
     else:
         return str(number)
     #raise NotImplementedError()
