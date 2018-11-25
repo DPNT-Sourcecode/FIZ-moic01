4d261efe237a5029e0612463cf335d0c9e6bcef7
sbx_user1059 <sbx_user1059@ip-10-128-128-221.eu-west-2.compute.internal>
2018-11-25 13:04:50 +0000

    Sun Nov 25 13:04:50 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,35 +1,35 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-	dlx1=number>10
-	dlx2=True
-	ns=str(number)
-	fr=ns[0]
-	for i in ns[1:]:
-		if i != fr:
-			dlx2=False
-			break
-	div3=number%3==0
-	div5=number%5==0
-	dlx= dlx1 and dlx2
-	in3=str(3) in str(number)
-	in5=str(5) in str(number)	
-	if (div3 or in3) and (div5 or in5):
-		if dlx:
-		   return "fizz buzz deluxe"
-		else:
-		   return "fizz buzz"
-	elif div3 or in3:
-	    if dlx:
-		  return "fizz deluxe"
-		else:
-		  return "fizz"
-	elif div5 or in5:
-	    if dlx:
-		  return "buzz deluxe"
-		else:
-		  return "buzz"
-	elif dlx:
-		return "deluxe"
-	else:
-		return str(number)
+    dlx1=number>10
+    dlx2=True
+    ns=str(number)
+    fr=ns[0]
+    for i in ns[1:]:
+        if i != fr:
+            dlx2=False
+            break
+    div3=number%3==0
+    div5=number%5==0
+    dlx= dlx1 and dlx2
+    in3=str(3) in str(number)
+    in5=str(5) in str(number)	
+    if (div3 or in3) and (div5 or in5):
+        if dlx:
+            return "fizz buzz deluxe"
+        else:
+            return "fizz buzz"
+    elif div3 or in3:
+        if dlx:
+            return "fizz deluxe"
+        else:
+            return "fizz"
+    elif div5 or in5:
+        if dlx:
+            return "buzz deluxe"
+        else:
+            return "buzz"
+    elif dlx:
+        return "deluxe"
+    else:
+        return str(number)
     #raise NotImplementedError()
