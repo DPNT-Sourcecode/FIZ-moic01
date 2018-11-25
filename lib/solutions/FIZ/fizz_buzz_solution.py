a147b361a2d8daef101f3ddad0d4a9d4a733ab85
sbx_user1059 <sbx_user1059@ip-10-128-128-221.eu-west-2.compute.internal>
2018-11-25 12:39:23 +0000

    Sun Nov 25 12:39:23 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,11 +1,24 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
+	dxl1=number>10
+	ns=str(number)
+	fr=ns[0]
+	for i in ns[1:]:
+		if i!=fr:
+			dlx2=False
+			break
 	div3=number%3==0
 	div5=number%5==0
+	dlx= dlx1 and dlx2
 	in3=str(3) in str(number)
-	in5=str(5) in str(number)
+	in5=str(5) in str(number)	
 	if (div3 or in3) and (div5 or in5):
-		return "fizz buzz"	
+		if dlx:
+			return "fizz buzz deluxe"
+		else:
+			return "fizz buzz"
+	elif dlx:
+		return "deluxe"
 	elif div3 or in3:
 		return "fizz"
 	elif div5 or in5:
