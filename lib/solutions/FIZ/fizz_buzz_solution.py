74a4034187ade539f9d2f2779ba86af5d91335e6
sbx_user1059 <sbx_user1059@ip-10-128-128-221.eu-west-2.compute.internal>
2018-11-25 12:20:52 +0000

    Sun Nov 25 12:20:52 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,10 +1,11 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
+	a=str(number)
 	if number%3==0 and number%5==0:
 		return "fizz buzz"	
-	elif number%3==0:
+	elif number%3==0 or "3" in a:
 		return "fizz"
-	elif number%5==0:
+	elif number%5==0 or "5" in a:
 		return "buzz"
 	else:
 		return str(number)
